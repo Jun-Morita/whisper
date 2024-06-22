252d6954c7a63b4a92549fadfea4cf488e943aaf
Jun-Morita <m.junichiro@gmail.com>
2024-06-22 11:58:51 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # whisper
+
+## 環境設定
+
+conda create -n whisper python=3.9
+
+conda install -c anaconda numpy
+conda install -c anaconda pandas
+
+conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+
+(CPU版)
+conda install pytorch torchvision torchaudio cpuonly -c pytorch
+
+conda install -c anaconda ipykernel
+
+conda install ffmpeg -c conda-forge
+
+pip install git+https://github.com/openai/whisper.git
+
+pip install streamlit
+
+## Ref.
+https://github.com/openai/whisper
---
+++ whisper_main.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import whisper
+import tempfile
+import warnings
+
+warnings.filterwarnings(
+    "ignore", category=UserWarning, 
+    message="FP16 is not supported on CPU; using FP32 instead")
+
+# アプリのタイトルを設定
+st.title("音声解析アプリ")
+
+# サイドバーでモデルサイズを選択
+model_size = st.sidebar.selectbox(
+    "Whisperモデルサイズを選択してください",
+    ("large", "medium", "small", "base", "tiny")
+)
+
+# Whisperモデルの読み込み
+model = whisper.load_model(model_size)
+
+# 音声ファイルのアップロード
+audio_file = st.file_uploader(
+    "音声ファイルをアップロードしてください", 
+    type=["m4a", "mp3", "webm", "mp4", "mpga", "wav"])
+
+if audio_file is not None:
+    if st.button("音声文字起こしを実行する"):
+        with st.spinner("音声文字起こしを実行中です..."):
+            # 一時ファイルとして保存
+            with tempfile.NamedTemporaryFile(delete=False) as temp_audio_file:
+                temp_audio_file.write(audio_file.read())
+                temp_audio_path = temp_audio_file.name
+
+            # Whisperモデルを使って音声をテキストに変換
+            result = model.transcribe(temp_audio_path)
+            
+            # 変換結果を表示
+            st.success("音声文字起こしが完了しました！")
+            st.text_area("文字起こし結果", result["text"], height=300)
+
+            # テキストをファイルとして保存
+            text_file_path = tempfile.mktemp(suffix=".txt")
+            with open(text_file_path, "w") as text_file:
+                text_file.write(result["text"])
+
+            # ダウンロードボタンの作成
+            with open(text_file_path, "rb") as text_file:
+                st.download_button(
+                    label="文字起こし結果をダウンロード",
+                    data=text_file,
+                    file_name="transcription.txt",
+                    mime="text/plain"
+                )
